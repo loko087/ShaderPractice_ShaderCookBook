7530ea119dd86a9b64205c545bac04a38f396afa
Jose Monge <jmonge@headlesschickengames.com>
2017-10-29 22:11:43 -0600

    Finished Chapter 2

---
+++ Assets/MY_SHADERS/CH02/TerrainShader/RadiusShaderAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadiusShaderAnimation : MonoBehaviour {
+
+
+	public Material radiusJP;
+	public float radius;
+	public Color radiusColor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		radiusJP.SetVector("_Center",transform.position);
+		radiusJP.SetColor("_CircleColor",radiusColor);
+		radiusJP.SetFloat("_Radius",radius);
+	}
+}
